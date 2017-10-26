95879379aaa4134ca7e639d9e8e9c92f9486dbd7
Hugo-Paris <hugoplho@gmail.com>
2017-10-25 21:51:21 -0700

    minesweeper.js v2 and ES5 transpiled files

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,20 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine  = '  | B |  ';
-console.log("This is what an empty board would look like:");
-console.log(`${blankLine}`);
-console.log("This is what a board with a guess and a bomb on it would look like:")
-console.log(`${blankLine}`);
-console.log(`${guessLine}`);
-console.log(`${bombLine}`);
+const printBoard = (board) => {
 
+
+	console.log('current board:');
+	console.log(board[0].join('|'));
+	console.log(board[1].join('|'));
+	console.log(board[2].join('|'));
+} 
+
+let board = [ 	
+				[' ', ' ', ' '], 
+				[' ', ' ', ' '], 
+				[' ', ' ', ' ']   
+			];
+
+printBoard(board);
+
+board[0][1] = 1;
+board[1][2] = 'B';
+printBoard(board);
