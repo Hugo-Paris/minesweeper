c7a6c4dde852240d941ac3615d6807e27a6c9276
Hugo-Paris <hugoplho@gmail.com>
2017-10-30 14:26:27 -0700

    number of neighboring bombs get returned, when not landing on a bomb

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -39,7 +39,7 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 		board.push(row);
 	}
 
-	let numberOfBombsPlaced=0;
+	let numberOfBombsPlaced = 0;
 	let randomRowIndex;
 	let randomColIndex;
 	
@@ -48,22 +48,69 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 		randomRowIndex = Math.floor(Math.random()*numberOfRows);
 		randomColIndex = Math.floor(Math.random()*numberOfColumns);
 		// prevent duplicate
-		while(board[randomRowIndex][randomColIndex] == 'B'){
-			randomRowIndex = Math.floor(Math.random()*numberOfRows);
-			randomColIndex = Math.floor(Math.random()*numberOfColumns);			
+		if(board[randomRowIndex][randomColIndex] != 'B'){
+			board[randomRowIndex][randomColIndex] = 'B';
+			numberOfBombsPlaced++;		
 		}
-		board[randomRowIndex][randomColIndex] = 'B';
-		numberOfBombsPlaced++;
 	}
 
 	return board;	
 };
 
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, colIndex) => {
+	const neighborOffsets =[ 	[-1, -1],
+								[-1,  0],
+								[-1,  1],
+								[ 0, -1],
+								[ 1, -1],
+								[ 1,  0],
+								[ 0,  1],
+								[ 1,  1]
+							];
+	const numberOfRows 		= bombBoard.length;												
+	const numberOfColumns 	= bombBoard[0].length;	
+	let numberOfNighboringBombs = 0;			
+
+	neighborOffsets.forEach(offset => {
+		let neighborRowIndex = rowIndex + offset[0];
+		let neighborColIndex = colIndex + offset[1];
+		if( (neighborRowIndex>=0) && (neighborColIndex>=0) && (neighborRowIndex < numberOfRows) && (neighborColIndex < numberOfColumns) ){
+			if (bombBoard[neighborRowIndex][neighborColIndex] === 'B'){
+				numberOfNighboringBombs++;
+			}
+		}
+	});	
+	return numberOfNighboringBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, colIndex) => {
+	// Has the specified tile already been flipped?
+	// check that tile isn't emty
+	if (playerBoard[rowIndex][colIndex] !== ' ' ){
+		console.log('This tile has already been flipped')
+		return;
+	}
+	// check that tile is bombed
+	else if (bombBoard[rowIndex][colIndex] === 'B' ){
+		playerBoard[rowIndex][colIndex] = 'B';
+	}
+	// return number of neighboring bombs
+	else{
+		playerBoard[rowIndex][colIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, colIndex);
+	}
+}
+
 
 let playerBoard = generatePlayerBoard(3,4);
-let bombBoard 	= generateBombBoard(3,4, 8);
+let bombBoard 	= generateBombBoard(3,4, 2);
 console.log("Player board:");
 printBoard(playerBoard);
 console.log("Bomb board:");
 printBoard(bombBoard);
+
+// first player entry
+flipTile(playerBoard, bombBoard, 0, 0);
+console.log('Updated player board: ');
+printBoard(playerBoard);
+
 
